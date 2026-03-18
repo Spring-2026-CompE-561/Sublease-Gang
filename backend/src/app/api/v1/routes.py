80302e7b916224152ebd297e4012933063653c8f
jadengong <ong.jadengabriel@gmail.com>
2026-03-17 17:43:00 -0700

    fix typo in /api/v1/routes.py for api_router

--- backend/src/app/api/v1/routes.py
+++ backend/src/app/api/v1/routes.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 
 from app.routes.conversations import router as conversations_router
 
-api-router = APIRouter()
+api_router = APIRouter()
 api_router.include_router(conversations_router)
