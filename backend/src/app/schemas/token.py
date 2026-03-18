6b904a66601dc06686d4a50697a54831949bda42
Dsneezy <dkawesomer@gmail.com>
2026-03-18 14:58:40 -0700

    separate different data inputs

--- backend/src/app/schemas/token.py
+++ backend/src/app/schemas/token.py
@@ -3,15 +3,23 @@ from typing import Optional, Field
 
 from pydantic import BaseModel
 
-class Token(BaseModel):
-    id: int
+class TokenBase(BaseModel):
     user_id: int 
-    created_at: datetime
     access_token: Optional[str] = None
     refresh_token: Optional[str] = None
     expiration_time: Optional[datetime] = None
     scope: Optional[str] = None
     token_type: str = Field(..., alias="tokenType")
+
+class TokenCreate(TokenBase):
+    pass
+
+class Token(TokenBase):
+    id: int
+    time_created: datetime
+
+    class Config:
+        from_attributes = True
 
 
 
