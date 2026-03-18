41cdf60482abe1c54db9aa3a03fbfc3473c8ca31
jadengong <ong.jadengabriel@gmail.com>
2026-03-17 17:34:28 -0700

    update main.py + models/init.py to include Token import

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import Base, engine
 from app.core.settings import settings
 
-from app.models import User, Conversation
+from app.models import User, Conversation, Token
 
 # create DB tables
 Base.metadata.create_all(bind=engine)
