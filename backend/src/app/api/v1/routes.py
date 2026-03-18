fa6270a0d2fc4587b7e4e2b48f6090f8fb4ea8b1
jadengong <ong.jadengabriel@gmail.com>
2026-03-17 18:30:47 -0700

    update /api/v1/routes.py to include prefix

--- backend/src/app/api/v1/routes.py
+++ backend/src/app/api/v1/routes.py
@@ -2,5 +2,5 @@ from fastapi import APIRouter
 
 from app.routes.conversations import router as conversations_router
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(conversations_router)
