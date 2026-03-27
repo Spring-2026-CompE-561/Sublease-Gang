58f81bbeed344e4b6891a65a5f8e96e25de5290d
Enzo Weiss <enzoweiss2014@gmail.com>
2026-03-26 18:16:59 -0700

    Update user.py

--- backend/src/app/models/user.py
+++ backend/src/app/models/user.py
@@ -12,7 +12,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String(254), unique=True, index=True, nullable=False)
     username = Column(String(50), unique=True, index=True, nullable=False)
-    password_hash = Column(String(255), nullable=False)
+    password_hash = Column(String(512), nullable=False)
     account_disabled = Column(Boolean, default=False) 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
