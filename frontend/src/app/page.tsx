6427e59466bf65003c5dd12a2a095298c39fde7c
bendennis06 <130309802+bendennis06@users.noreply.github.com>
2026-04-25 14:47:49 -0700

    add footer

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 
-
 import { Navbar } from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <main>
+    <div className = "flex min-h-screen flex-col">
       <Navbar />
-    </main>
+      <main className = "flex-1" />
+      <Footer />
+    </div>
   );
 }
