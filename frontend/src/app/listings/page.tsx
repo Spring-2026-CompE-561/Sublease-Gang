c99a9c0fd3720978acb6585ea955fc1c7bae1588
Dsneezy <dkawesomer@gmail.com>
2026-04-29 17:13:36 -0700

    working on filter

---
+++ frontend/src/app/listings/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { FilterPanel, MobileFilterSheet } from "@/components/FilterPanel";
+import { Filter } from "lucide-react";
+
+export default function ListingPlacePage() {
+  return (      
+    <div className="flex gap-6">
+        <aside className="hidden lg:block w-60 px">
+            <FilterPanel />
+        </aside>
+
+        <div className="lg:hidden">
+            <MobileFilterSheet />
+        </div>
+
+        <main className="container mx-auto flex flex-1 flex-col items-center justify-center px-4 py-16 text-center">
+            <h1 className="text-3xl font-semibold">View Listings</h1>
+        </main>
+    </div>
+  );
+}
