d4e9c0b20590be8759cec86355304d9a887722dd
bendennis06 <130309802+bendennis06@users.noreply.github.com>
2026-05-01 15:59:38 -0700

    Add CTA banner section to home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/home/Hero";
+import CtaBanner from "@/components/home/cta-banner";
 
 export default function Home() {
   //return null;
   return (
     <main className="flex-1">
-      <Hero />    
+      <Hero />
+      <CtaBanner />    
     </main>
     );
 }
