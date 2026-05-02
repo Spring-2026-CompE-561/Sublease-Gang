c9865d7cabb10f8749ff839f10b3216334cb50dd
Caleb Dickson <157729906+CalebDksn25@users.noreply.github.com>
2026-05-02 11:23:53 -0700

    Added map to nav bar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -39,6 +39,9 @@ function DesktopNavbar() {
           <Link href="/listings" className="text-sm">
             Browse
           </Link>
+          <Link href="/map" className="text-sm">
+            Map
+          </Link>
           <Link href="/list" className="text-sm">
             List your place
           </Link>
@@ -65,6 +68,9 @@ function MobileNavbar() {
               <div className="flex flex-col gap-2 pt-6">
                 <Link href="/listings" onClick={() => setIsOpen(false)}>
                   Browse listings
+                </Link>
+                <Link href="/map" onClick={() => setIsOpen(false)}>
+                  Map
                 </Link>
                 <Link href="/list" onClick={() => setIsOpen(false)}>
                   List your place
