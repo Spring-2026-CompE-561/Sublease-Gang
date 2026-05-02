ef8a92500b4661a5185024817de9d3f4a7116c03
bendennis06 <130309802+bendennis06@users.noreply.github.com>
2026-05-02 15:39:03 -0700

    added fake listings to my listings

--- frontend/src/components/UserButton.tsx
+++ frontend/src/components/UserButton.tsx
@@ -28,13 +28,13 @@ function UserButton() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         {isLoggedIn ? (
-          <>
-            <DropdownMenuItem onClick={() => router.push("/profile")}>
-              Profile
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={handleSignOut}>Sign out</DropdownMenuItem>
-          </>
-        ) : (
+        <>
+        <DropdownMenuItem onClick={() => router.push("/saved-listings")}>
+        Saved listings
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={handleSignOut}>Sign out</DropdownMenuItem>
+        </>
+    ) : (
           <>
             <DropdownMenuItem onClick={() => router.push("/signin")}>
               Sign in
