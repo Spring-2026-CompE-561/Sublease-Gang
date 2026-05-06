7854285ee14cc120708f112e21bc830e1003c3bb
Dsneezy <dkawesomer@gmail.com>
2026-05-06 13:45:24 -0700

    add router

--- backend/src/app/api/v1/routes.py
+++ backend/src/app/api/v1/routes.py
@@ -7,6 +7,7 @@ from app.routes.map import router as map_router
 from app.routes.profile import router as profile_router
 from app.routes.saved_listings import router as saved_listings_router
 from app.routes.users import router as users_router
+from app.routes.college import router as college_router
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(auth_router)
@@ -16,3 +17,4 @@ api_router.include_router(map_router)
 api_router.include_router(profile_router)
 api_router.include_router(saved_listings_router)
 api_router.include_router(users_router)
+api_router.include_router(college_router)
